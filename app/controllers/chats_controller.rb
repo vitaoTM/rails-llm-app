9bab937033dc34829e42bfe55e73a7288a6dcf8d
vitor <vitor.grosskopf@gmail.com>
2024-08-09 17:08:14 -0300

    Create turbo_stream file - Render chat obj created at controller #create - Update #index on chats_controller to redirect to last chat

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -3,7 +3,10 @@ class ChatsController < ApplicationController
 
   # GET /chats or /chats.json
   def index
-    @chats = Chat.all
+    @chats = Chat.none
+    if current_user.chats.any?
+      redirect_to chat_url(current_user.chats.last)
+    end
   end
 
   # GET /chats/1 or /chats/1.json
