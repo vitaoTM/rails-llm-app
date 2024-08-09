7a336c48c514862d31fa25d519365f7e63269cfa
vitor <vitor.grosskopf@gmail.com>
2024-08-09 16:08:34 -0300

    Update migration file - Update q_and_a column on migration set array default - Set index to chats on routes.rb - Add chats has_many associations to user - Ran: rails db:migrate

--- db/migrate/20240809185819_create_chats.rb
+++ db/migrate/20240809185819_create_chats.rb
@@ -3,7 +3,7 @@ class CreateChats < ActiveRecord::Migration[7.0]
     create_table :chats do |t|
       t.references :user, null: false, foreign_key: true
       t.json :history
-      t.string :q_a
+      t.string :q_and_a, array: true, default: []
 
       t.timestamps
     end
