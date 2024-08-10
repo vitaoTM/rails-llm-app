6d8353c91bcec35ce46c7026d1e95e927804c9e1
vitor <vitor.grosskopf@gmail.com>
2024-08-10 00:10:36 -0300

    Implement message assignment and conversation building with GPT-3 - Manages conversation history with message assignment. - Builds conversation using GPT-3 API call with specified parameters.

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,4 +2,43 @@ class Chat < ApplicationRecord
   belongs_to :user
 
   attr_accessor :message
+
+  def message=(message)
+    self.history = { 'prompt' => message, 'history' => [] } if history.blank?
+
+    messages = [
+    { 'role' => 'system', 'content' => history['prompt'] }
+    ]
+    q_and_a.each do |question, answer|
+      messages << { 'role' => 'user', 'content' => question }
+      messages << { 'role' => 'assistant', 'content' => answer }
+    end
+
+    messages << { 'role' => 'user', 'content' => message } if messages.size > 1
+
+    response_raw = client.chat(
+      parameters: {
+        model: 'gtp-3.5-turbo',
+        messages:,
+        temperature: 0.7,
+        max_tokens: 500,
+        top_p: 1,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.6
+      }
+    )
+
+    self.history['history'] << response_raw
+
+    Rails.logger.debug response_raw
+    response = JSON.parse(response_raw.to_json, object_class: OpenStruct)
+
+    self.q_and_a << [message, response.choices[0].message.content]
+  end
+
+  private
+
+  def client
+    OpenAI::Client.new
+  end
 end
