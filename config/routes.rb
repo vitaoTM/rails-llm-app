3f13cfdc9f9a6716e5db3ab19f31a82fe67a9656
vitor <vitor.grosskopf@gmail.com>
2024-08-12 16:20:46 -0300

    Add messages into chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :chats
+  resources :chats do
+    resources :messages
+  end
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
