f62a5d02701060d468ae39b4cf870476f9d6950c
vitor <vitor.grosskopf@gmail.com>
2024-08-12 22:34:36 -0300

    Implement message creation with role assignment

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,16 @@
 class MessagesController < ApplicationController
   def create
+    @chat = Chat.find(params[:chat_id])
+    role = @chat.messages.any? ? 'user ' : 'system'
+    @message = @chat.messages.create!(message_params.merge(role: role))
+    respond_to do |format|
+      format.turbo_stream
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
