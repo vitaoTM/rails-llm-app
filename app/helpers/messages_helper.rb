aee22e4dec234d8e47296bc0d3a052a0d3f9ae0c
vitor <vitor.grosskopf@gmail.com>
2024-08-13 00:47:25 -0300

    Adds bootstrap icons to message helper
    - Update partial message

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,12 @@
 module MessagesHelper
+  def icon_for(message)
+    case message.role
+    when 'system'
+      '<i class="fa-duotone fa-star"></i>'
+    when 'user'
+      '<i> class="fa-duotone fa-user"></i>'
+    when 'assistant'
+      '<i class="f-duotone fa-microchip-ai"></i>'
+    end
+  end
 end
