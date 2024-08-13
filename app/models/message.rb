90ede29ef07c500680b3616eaadedb1c05072353
vitor <vitor.grosskopf@gmail.com>
2024-08-13 00:39:48 -0300

    Adds broadcast method to message model - Include ActionView::RecordIdentifier to use dom_id without errors

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,25 @@
 class Message < ApplicationRecord
+  include ActionView::RecordIdentifier
   belongs_to :chat
+
+  after_create_commit -> { broadcast_createed }
+  after_update_commit -> { broadcast_updated }
+
+  def broadcast_createed
+    broadcast_append_to(
+      "#{dom_id(chat)}_messages",
+      partial: 'messages/message',
+      locals: { message: self, scroll_to: true },
+      target: "#{dom_id(chat)}_messages"
+    )
+  end
+
+  def broadcast_updated
+    broadcast_append_to(
+      "#{dom_id(chat)}_messages",
+      partial: 'messages/message',
+      locals: { message: self, scroll_to: true },
+      target: "#{dom_id(chat)}_messages"
+    )
+  end
 end
