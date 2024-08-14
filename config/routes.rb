51b636b23a68a606fac5c1763c812b70cb5d69cc
vitor <vitor.grosskopf@gmail.com>
2024-08-14 19:16:50 -0300

    Update routes - Add sidekiq web dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
+require "sidekiq/web"
 Rails.application.routes.draw do
+  mount Sidekiq::Web => "/sidekiq"
   resources :chats do
     resources :messages
   end
+
 
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
