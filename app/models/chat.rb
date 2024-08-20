4a45465de0776002769d54f71d190f0f78cbb4fa
vitor <vitor.grosskopf@gmail.com>
2024-08-20 16:15:31 -0300

    Adds scope to has_many association - order chats by messages ids - Source: <https://api.rubyonrails.org/v7.0/classes/ActiveRecord/Associations/ClassMethods.html\#method-i-has_many\>

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,7 +1,7 @@
 class Chat < ApplicationRecord
   belongs_to :user
 
-  has_many :messages, dependent: :destroy
+  has_many :messages, -> { order(:id) } ,dependent: :destroy
 
   def message=(message)
     self.history = { 'prompt' => message, 'history' => [] } if history.blank?
